2aa55d7d5d1d43d3607e2dbc902ba1378089d44d
unsiliquaria <pancito.couperin@gmail.com>
2023-05-28 15:45:52 -0400

    File(s) deleted

---
+++ rsp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+## Hola, Edu. Para usar este script, primero abre el terminal y cámbiate al directorio donde descargaste el archivo, escribiendo:
+## cd ~/Carpeta/Carpeta/
+## Para escribir una carpeta, puedes presionar dos veces Tab para que te aparezcan a las que puedes ir, o puedes hacer lo mismo luego
+## de escribir las primeras letras del nombre del directorio para que se autocomplete y no lo tengas que escribir letra por letra.
+## Luego, escribe esto para hacer ejecutable el script:
+## sudo chmod +x ./rsp.sh
+## Te pedirá la contraseña de tu mac.
+## Luego, para ejecutar el script, escribe:
+## ./rsp.sh
+## Y listo. Cualquier problema me avisas.
+
+git clone https://github.com/delibellus/crawler.git
+echo -e '\n\nAhora te pedirá la contraseña de tu mac para instalar un gestor que te permitirá subir aportes sin tener que escribir tus credenciales:'
+sudo brew install gh
+echo -e '\n\nAhora te pedirá escoger un protocolo. Escoge "https":'
+gh auth login
+echo -e '\n\nEsto te pedirá tu nombre de usuario en github y una contraseña que te dejé en el whatsapp:'
+git push origin main
+
+if [[ -e ~/.bashrc ]]; then
+    echo "alias rsp='git add -A && git commit -m && git push origin main'" >> ~/.bashrc
+else
+    echo "#!/bin/bash" > ~/.bashrc && echo "alias rsp='git add -A && git commit -m && git push origin main'" >> ~/.bashrc
+fi
+
+source ~/.bashrc
+
+echo -e '\n\nAhora, cada vez que subas un aporte, puedes escribir solo "rsp" y se subirá, siempre y cuando te encuentres en la carpeta del repositorio.\nCada vez que lo hagas, te pedirá especificar qué aportaste o modificaste.'
